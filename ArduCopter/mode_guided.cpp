5f52460bc72c893fec53bd601e53a2f70d9c4705
nufeichuiyun <54277734+nufeichuiyun@users.noreply.github.com>
2020-04-27 01:15:35 +0800

    解决数组下标问题 仿真通过

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -88,7 +88,8 @@ void ModeGuided::pos_control_start()
     // wp_nav->get_wp_stopping_point(stopping_point);
 
     // no need to check return status because terrain data is not used
-    wp_nav->set_wp_destination(copter.user_waypoint[copter.current_user_waypoint_num], false);
+    wp_nav->set_wp_destination(copter.user_waypoint[copter.current_user_waypoint_num-1], false);
+    copter.current_user_waypoint_num --;
 
     // initialise yaw
     auto_yaw.set_mode_to_default(false);
@@ -415,11 +416,10 @@ void Mode::auto_takeoff_run()
 // called from guided_run
 void ModeGuided::pos_control_run()
 {
-    if(copter.current_user_waypoint_num >= 0){
-        if(wp_nav->reached_wp_destination()){
+    if(wp_nav->reached_wp_destination()){
+        if(copter.current_user_waypoint_num-1 >= 0){
+            wp_nav->set_wp_destination(copter.user_waypoint[copter.current_user_waypoint_num-1], false);
             copter.current_user_waypoint_num --;
-            if(copter.current_user_waypoint_num >= 0)
-                wp_nav->set_wp_destination(copter.user_waypoint[copter.current_user_waypoint_num], false);
         }
     }
 
